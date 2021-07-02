8845e627993c15fe56ade362f502242ff52e45f2
aachalmakode <aachalmakode900@gmail.com>
2021-07-02 17:52:04 -0400

    CreatStack

--- src/com/SQ/MianClass.java
+++ src/com/SQ/MianClass.java
@@ -2,6 +2,15 @@ package com.SQ;
 
 //main method
 class MainClass {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        StackQueue stackQueue=new StackQueue();
+        stackQueue.push(70);
+        stackQueue.push(30);
+        stackQueue.push(56);
+        stackQueue.print();
+
 
     }
 
+}
--- src/com/SQ/StackQueue.java
+++ src/com/SQ/StackQueue.java
@@ -1,5 +1,43 @@
 package com.SQ;
 public class StackQueue {
 
+    //creating head node
+    Node head;
+
+    class Node{
+        Node next;
+        int data;
+
+
+        //constructor
+        public Node(int data) {
+            super();
+            this.next = next;
+            this.data = data;
+        }
+    }
+
+    //uc1 insert data in stack
+    public void push(int data)
+    {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+    //display stack
+    public void print()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+
+
+
 
 }
